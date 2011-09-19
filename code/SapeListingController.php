e8c8ab6aa3c289bf6a315d465a054f6b666f37e7
Robin Petterd <robin@sproutlabs.com.au>
2011-09-19 16:45:48 +1000

    Revert e9189121f00c5e23bb148f214382684f57cb3309^..HEAD

--- code/SapeListingController.php
+++ code/SapeListingController.php
@@ -14,11 +14,11 @@ abstract class SapeListingController extends FacetedListingController {
     public function init() {
 		parent::init();
                                 
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery-metadata/jquery.metadata.js');
+		 Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		 Requirements::javascript(THIRDPARTY_DIR . '/jquery-metadata/jquery.metadata.js');
 		//CHANGE so that we use the local .js so that Google DataTables can be made. 
-                Requirements::javascript('https://www.google.com/jsapi');
-                Requirements::javascript('sape/javascript/SapeListing.js');
+                 Requirements::javascript('http://www.google.com/jsapi');
+                 Requirements::javascript('sape/javascript/SapeListing.js');
 	}
 
       
